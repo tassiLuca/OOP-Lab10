897369dcf96dfa926135fd0ec8ad6732ea99c3b0
Luca Tassinari - luca.tassinari10@studio.unibo.it <luca.tassinari10@studio.unibo.it>
2020-11-30 11:01:44 +0100

    CuncurrentGUI created

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,9 @@
+package it.unibo.oop.lab.reactivegui02;
+
+import javax.swing.JFrame;
+
+public class ConcurrentGUI extends JFrame {
+
+    private static final long serialVersionUID = -8630968055862320453L;
+
+}
