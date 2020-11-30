63d69d39d55f46e1056d76934da91445f708b190
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-11-30 11:57:50 +0100

    Removed superflous imports

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -2,14 +2,11 @@ package it.unibo.oop.lab.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
